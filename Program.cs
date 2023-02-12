056db59ec8d349a1d392e3adc566d540f4b15388
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-02-12 14:44:17 +0530

    Added files to remote

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeePayrollServiceSQL
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+             
+            Console.WriteLine("Welcome To Employee Payroll Using Thread Problem");
+            List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
+            EmployeePayrollOperations employeePayrollOperations = new EmployeePayrollOperations();
+            DateTime startDateTime = DateTime.Now;
+            employeePayrollOperations.addEmployeeToPayroll(employeeDetails);
+            DateTime stopDateTime = DateTime.Now;
+            Console.WriteLine("Duration without thread: " + (stopDateTime - startDateTime));
+
+            DateTime startDateTimeThread = DateTime.Now;
+            employeePayrollOperations.addEmployeeToPayrollWithThread(employeeDetails);
+            DateTime stopDateTimeThread = DateTime.Now;
+            Console.WriteLine("Duration with Thread: " + (stopDateTimeThread - startDateTimeThread));
+
+
+        
+
+        }
+    }
+}
